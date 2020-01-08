499483ba2a02e9ed72cb4c2fe7d7226533186fb2
Dhruvil Mody <dhruvil.mody@somaiya.edu>
2020-01-08 14:43:24 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,30 @@
+print "......Comments......\n"
+print 'Single Line ' "#"
+print'Multi Line ("  """  ")'
+
+print '\n......Datatypes......\n'
+a=10 		#integer datatype
+b=10.10 	#float datatype
+c="abc" 	#String datatype
+d=a+b 		#expression
+g=2.3+3.2j	#complex datatype
+
+print "\n......Expressions......\n"
+print "c=10"
+print "d=20"
+print "e=c+d"
+print "f=c-d"
+#print "sum is:",e
+#print "difference is:",f
+
+d=20
+print "Decimal:",d
+f=float(d)		#convert into float
+print "Integer to float",f 
+h=oct(d)		#convert into octal
+print "Decimal to octal",h
+m=bin(d)		#convert into binary
+print "Decimal to binary",m
+n=hex(d)		#convert into hex
+print "Decimal to hex",n
+
